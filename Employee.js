2510a30fb97eb373ac079fa704e7904ad01da953
aadarsh <aadarshg2562003@example.com>
2025-03-04 19:24:56 +0530

    [Aadarsh] Add . UC-2 feature Calculate Employee Wage using Switch Case

--- Employee.js
+++ Employee.js
@@ -7,3 +7,29 @@ if (empCheck === IS_ABSENT) {
 } else {
     console.log("Employee is PRESENT");
 }
+
+//Calculate Daily Employee Wage based on Work Type
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+// Generate a random number (0, 1, or 2) to determine employee work type
+let empCheck2 = Math.floor(Math.random() * 10) % 3;
+
+let empHrs = 0;
+
+switch (empCheck2) {
+    case IS_PART_TIME:
+        empHrs = PART_TIME_HOURS;
+        break;
+    case IS_FULL_TIME:
+        empHrs = FULL_TIME_HOURS;
+        break;
+    default:
+        empHrs = 0; // No work
+}
+
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Employee Wage: $" + empWage);
